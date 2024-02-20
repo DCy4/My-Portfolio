e9c7cacf41c11768c762eb1dacd8e1e69b7274b3
David Calle <davidacalle@gmail.com>
2024-02-20 16:55:31 +0000

    Created templates , continue at TimelineItem Component

---
+++ minimal-portfolio/src/components/TimelineItem.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function TimelineItem({year, title, duration, details}) {
+    return ( 
+        <div>Timeline Item</div>
+     );
+}
+
+export default TimelineItem;
---
+++ minimal-portfolio/src/data/portfolio.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        title: 'Weather App',
+        imgUrl: 'public/assets/san-fran.jpg',
+        stack: ['React', 'JavaScript'],
+        link: 'Link to website'
+    }, 
+    {
+        title: 'Developer Portfolio',
+        imgUrl: 'public/assets/images.png',
+        stack: ['React', 'Tailwind', 'Vite'],
+        link: 'Link to website'
+    },
+    {
+        title: 'US States Guessing Game',
+        imgUrl: '/public/assets/blank_states_img.gif',
+        stack: ['Python', 'turtle'],
+        link: 'https://github.com/DCy4/US-States-Game'
+    }
+]
