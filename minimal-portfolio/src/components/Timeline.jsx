91589d30be5c293c36cb2784e0ba0edd55b37232
David Calle <davidacalle@gmail.com>
2024-03-12 18:48:50 +0000

    Timeline

--- minimal-portfolio/src/components/Timeline.jsx
+++ minimal-portfolio/src/components/Timeline.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import timeline from "../data/timeline";
+
 
 function Timeline() {
     return ( 
-        <div>
-           Timeline 
+        <div className="flex flex-column md:flex-row justify-center my-20
+        w-full md:w-7/12">
+           {/* <div className="w-full md:"></div> */}
         </div>
      );
 }
